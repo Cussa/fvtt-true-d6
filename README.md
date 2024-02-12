47a00f4cd8f33b8b52929d82b2bff6d12f6eb345
Cussa Mitre <cussa.mitre@gmail.com>
2024-02-12 23:44:38 +0000

    🎉 Project initialized

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# fvtt-true-d6
+# TRUE-d6
 An OFFICIAL Foundry implementation for the TRUE-d6 System!
+TRUE-d6: created by Lucio Gozzi / Nerdura Games
+Implemented by: CussaMitre
+
+To acquire TRUE-d6: https://preview.drivethrurpg.com/en/product/454719/TRUEd6
---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const TRUED6 = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+TRUED6.abilities = {
+  str: 'TRUED6.Ability.Str.long',
+  dex: 'TRUED6.Ability.Dex.long',
+  con: 'TRUED6.Ability.Con.long',
+  int: 'TRUED6.Ability.Int.long',
+  wis: 'TRUED6.Ability.Wis.long',
+  cha: 'TRUED6.Ability.Cha.long',
+};
+
+TRUED6.abilityAbbreviations = {
+  str: 'TRUED6.Ability.Str.abbr',
+  dex: 'TRUED6.Ability.Dex.abbr',
+  con: 'TRUED6.Ability.Con.abbr',
+  int: 'TRUED6.Ability.Int.abbr',
+  wis: 'TRUED6.Ability.Wis.abbr',
+  cha: 'TRUED6.Ability.Cha.abbr',
+};
